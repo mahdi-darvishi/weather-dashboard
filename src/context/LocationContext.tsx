8865cf54934e90c93f86662743436b9852b2db22
mahdi-darvishi <mehdidarvishi2004@gmail.com>
2025-11-09 20:02:41 +0330

    feat: update location selection logic and improve dashboard layout; enhance login page with dynamic background images

--- src/context/LocationContext.tsx
+++ src/context/LocationContext.tsx
@@ -6,7 +6,7 @@ import { locationsList } from "../data/lcoation";
 
 export const LocationProvider = ({ children }: { children: ReactNode }) => {
   const [selectedLocation, setSelectedLocation] =
-    useState<LocationOption | null>(locationsList[0] || null);
+    useState<LocationOption | null>(locationsList[32] || null);
 
   const setLocation = (location: LocationOption | null) => {
     setSelectedLocation(location);
